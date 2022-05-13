405b892a1654e9531d0a4cf3e67332acdfab2a1b
shifumy <ymblandin@gmail.com>
2022-05-13 10:06:24 +0200

    create the pages and the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Test from "./pages/Test";
+import Results from "./pages/Results";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
