abdb34f2d4217a6021a7331f07713d17aebdf594
piment <romain@mura.io>
2022-05-13 15:48:14 +0200

    refactoring

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
   }, []);
   return (
     <div className="home">
-      <img src={dog.img} alt="dog image" />
+      <img src={dog.img} alt="dog" />
       <h2>{dog.name}</h2>
     </div>
   );
