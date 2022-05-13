8816c4d71ad74b4855497159fae79bf8d4a9a9b2
shifumy <ymblandin@gmail.com>
2022-05-13 15:22:26 +0200

    change the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
 import { IdContext } from "../contexts/IdContext";
 import "../assets/css/home.css";
 
 export default function Home() {
   const { dogId } = useContext(IdContext);
-  return <div className="home">{dogId}</div>;
+  const [dogInfo, setDogInfo] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/dogs/${dogId}`)
+      .then((response) => setDogInfo(response.data));
+  }, []);
+
+  return (
+    <div className="home">
+      <p>{dogInfo[0]?.name}</p>
+      <NavLink to="/test">
+        <button type="button"> Démarrer les tests</button>
+      </NavLink>
+    </div>
+  );
 }
